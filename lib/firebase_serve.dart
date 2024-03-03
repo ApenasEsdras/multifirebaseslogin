43eabd22a0070aec045368b7921fa3193ec9b92e
esdras.de.oliveira <apenasesdras@gmail.com>
2024-03-03 07:33:20 -0300

    ajustes

---
+++ lib/firebase_serve.dart
@@ -0,0 +1,23 @@
+
+import 'package:firebase_core/firebase_core.dart';
+Map<String, FirebaseOptions> firebaseConfigs = {
+  'projeto1': const FirebaseOptions(
+    apiKey: '',
+    appId: '',
+    messagingSenderId: '',
+    projectId: '',
+    authDomain: '',
+    storageBucket: '',
+    measurementId: '',
+  ),
+  'projeto2': const FirebaseOptions(
+    apiKey: '',
+    appId: '',
+    messagingSenderId: '',
+    projectId: '',
+    authDomain: '',
+    storageBucket: '',
+    measurementId: '',
+  ),
+  // Adicione configurações para mais projetos, se necessário
+};
